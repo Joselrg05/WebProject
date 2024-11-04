843655f17adf8a28cda0cd2c35a1874b5b8bda1d
José Luis Rodríguez Gutiérrez <jr7035960@gmail.com>
2024-11-03 22:47:46 -0600

    First Commit

--- InvenSyncWeb/App_Start/BundleConfig.cs
+++ InvenSyncWeb/App_Start/BundleConfig.cs
@@ -28,7 +28,8 @@ namespace InvenSyncWeb
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.bundle.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
         }
     }
 }
