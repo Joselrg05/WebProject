739a1917c525ffcaf58ad77ce02c69b2f6f5fe90
joselrg_ <jr7035960@gmail.com>
2024-11-04 02:31:13 -0600

    #update_package_bootstrap

--- InvenSyncWeb/App_Start/BundleConfig.cs
+++ InvenSyncWeb/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace InvenSyncWeb
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            bundles.Add(new Bundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new Bundle("~/bundles/otros").Include(
@@ -28,8 +28,7 @@ namespace InvenSyncWeb
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.bundle.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
         }
     }
 }
